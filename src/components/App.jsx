5cd1ebd720a6493ac6d41f4054d589eff34a7bdd
PolinaFaustova <polinamusikerin@gmail.com>
2023-05-14 22:49:28 +0200

    start working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, {Component} from "react";
+
+export class App extends Component {
+
+  state = {
+    
+  }
+
+  // componentDidMount() {
+  //   this.setState({loading: true});
+
+  //   fetch('https://pixabay.com/api/?q=cat&page=1&key=34956140-3027e5abb464d64a1ffd53e5a&image_type=photo&orientation=horizontal&per_page=12')
+  //   .then(res => res.json())
+  //   .then(pixabay => this.setState({pixabay}))
+  //   .finally(()=> this.setState({loading: false}));
+
+  // }
+
+  render() {
+
+    return (
+    <>
+    {/* {this.state.pixabay && (<div>{this.state.pixabay.name}</div>)}
+    {this.state.loading && <h1>Loading</h1>} */}
+    {/* <Searchbar />
+    <ImageGallery/>
+    <ImageGalleryItem/>
+    <Loader/>
+    <Button/>
+    <Modal/> */}
+
+    </>
+    )
+    
+  }
+  
 };
